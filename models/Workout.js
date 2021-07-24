15289b01576e855f7460ab9a30bfa7784823a88f
Tom Capers <tompcapers@gmail.com>
2021-07-24 13:09:03 -0700

    updated workout model and server get workout route

--- models/Workout.js
+++ models/Workout.js
@@ -2,17 +2,42 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
+
+
 const WorkoutSchema = new Schema({
-  name: {
-    type: String,
-    unique: true
+  day: {
+    type: Date,
+    default: Date.now
   },
   exercises: [
     {
-      type: Schema.Types.ObjectId,
-      ref: "Exercise"
+      type:{
+        type: String
+      } ,
+      name: {
+        type: String
+      },
+      weight: {
+        type: Number
+      },
+      sets:{
+        type: Number
+      },
+      reps: {
+        type: Number
+      },
+      duration:{
+        type: Number
+      },
+      distance:{
+        type: Number
+      }
+    
+    
     }
   ]
+  
+  
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
+
+
 const PORT = process.env.PORT || 3000;
 
-const db = require("./models");
+
+
+const db = require("./models")
 
 const app = express();
 
@@ -15,17 +19,31 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/custommethoddb", { useNewUrlParser: true });
 
 
-// db.Library.create({ name: "Campus Library" })
-//   .then(dbLibrary => {
-//     console.log(dbLibrary);
-//   })
-//   .catch(({message}) => {
-//     console.log(message);
-//   });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
+
+app.get("/workouts", (req,res) => {
+  db.Workout.find({})
+  .then(dbWorkout => {
+    console.log(dbWorkout)
+    res.json(dbWorkout);
+  })
+  .catch(err => {
+    res.json(err);
+  })
+})
+
+app.post("/workouts", (req,res) => {
+  db.Workout.create({})
+  .then(dbWorkout => {
+    res.json(dbWorkout);
+  })
+  .catch(err => {
+    res.json(err);
+  })
+})
 
 
 app.listen(PORT, () => {
